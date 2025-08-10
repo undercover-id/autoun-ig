cb854cba9f38aba49ba2a483b1b06767ab4f7486
undercover-id <vick.undercover@gmail.com>
2025-08-10 14:29:36 +0700

    Delete run-auto.py

--- run-auto.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-from nacl.secret import SecretBox
-pw = input("Masukkan password: ")
-key = hashlib.sha256(pw.encode()).digest()[:32]
-box = SecretBox(key)
-with open("vick.bin","rb") as f:
-    data = f.read()
-code = box.decrypt(data)
-exec(code.decode())
